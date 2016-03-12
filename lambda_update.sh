c614df3533f546b2cf36e08995e90749718f7eab
billykeyss <billhuang@live.ca>
2016-03-12 17:29:06 -0500

    REMOVE TEST FUNCTIONS

--- lambda_update.sh
+++ lambda_update.sh
@@ -2,9 +2,9 @@
 rm update.zip
 zip -r update.zip *
 aws lambda update-function-code \
---function-name test \
+--function-name RapBattle \
 --zip-file fileb://./update.zip
 aws lambda update-function-configuration \
---function-name test \
+--function-name RapBattle \
 --description "$(date) -- $(whoami)"
 rm update.zip
